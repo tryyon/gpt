54d6805e158fedd13d5d5df0d08ba3ee2f9dbea8
Vedavrat <vedavratpp@gmail.com>
2024-09-29 21:29:30 +0530

    feat: initial mui setup

---
+++ src/app/theme/typography.ts
@@ -0,0 +1,9 @@
+export const fontSize = 14;
+
+export const typography = {
+  fontSize,
+  htmlFontSize: 16,
+  fontFamily: "./fonts/GeistVF.woff",
+  body1: { fontSize },
+  body2: { fontSize },
+};
